accc91d07089ef7f37071cc3e493110e173f6366
llunet001 <llunet001@bordeaux-inp.fr>
2024-10-14 14:15:44 +0200

    :construction: added loop for chat after join

--- src/client.c
+++ src/client.c
@@ -6,7 +6,6 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
-#define MAX_USERS 100
 
 typedef struct
 {
@@ -14,9 +13,9 @@ typedef struct
     char username[50];
 } User;
 
-User users[MAX_USERS];
-int user_count = 0;
-char current_user[50] = ""; // Store the current user's username
+char current_user[50] = "";
+int is_in_group = 0;
+char group_name[50] = "";
 
 void send_command(int sockfd, const char *command)
 {
@@ -43,7 +42,7 @@ void send_command(int sockfd, const char *command)
     }
 }
 
-void handle_login_command(int sockfd, const char *command, char *current_user)
+void handle_login_command(int sockfd, char *command)
 {
     // Send login command to the server
     if (send(sockfd, command, strlen(command), 0) == -1)
@@ -61,7 +60,8 @@ void handle_login_command(int sockfd, const char *command, char *current_user)
         printf("Server response: %s\n", buffer);
         if (strncmp(buffer, "Login successful", 16) == 0)
         {
-            sscanf(command, "login %s", current_user); // Store the username
+            sscanf(command, "login %s", current_user);
+            printf("logged in as %s\n", current_user);
         }
     }
     else if (nbytes == 0)
@@ -72,6 +72,141 @@ void handle_login_command(int sockfd, const char *command, char *current_user)
     else
     {
         perror("recv");
+    }
+}
+
+void handle_chat(int sockfd, char *command)
+{
+    printf("you are now in : %s", group_name);
+    while (1)
+    {
+        printf("Enter message (type 'exit' to leave group): ");
+        if (fgets(command, BUFFER_SIZE, stdin) == NULL)
+        {
+            break;
+        }
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strncmp(command, "exit", 4) == 0)
+        {
+            printf("Leaving group...\n");
+            is_in_group = 0;
+            break;
+        }
+
+        if (send(sockfd, command, strlen(command), 0) == -1)
+        {
+            perror("send");
+            break;
+        }
+
+        char buffer[BUFFER_SIZE];
+        int nbytes = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+        if (nbytes > 0)
+        {
+            buffer[nbytes] = '\0';
+            printf("Server response: %s\n", buffer);
+        }
+        else if (nbytes == 0)
+        {
+            printf("Server closed the connection\n");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            perror("recv");
+            break;
+        }
+    }
+}
+
+void handle_join_command(int sockfd, char *command)
+{
+    if (strlen(current_user) == 0)
+    {
+        printf("You must be logged in to join a group.\n\n");
+    }
+    else
+    {
+        char join_command[BUFFER_SIZE];
+        snprintf(join_command, sizeof(join_command), "join_group %s %s", current_user, command + 11);
+
+        if (send(sockfd, join_command, strlen(join_command), 0) == -1)
+        {
+            perror("send");
+            return;
+        }
+    }
+    char buffer[BUFFER_SIZE];
+    int nbytes = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+
+    if (nbytes > 0)
+    {
+        buffer[nbytes] = '\0';
+        buffer[strcspn(buffer, "\n")] = '\0';
+        printf("Server response: %s\n", buffer);
+        if (strncmp(buffer, "Joined group successfully", 25) == 0)
+        {
+            sscanf(command, "join_group %s", group_name);
+            handle_chat(sockfd, command);
+        }
+    }
+    else if (nbytes == 0)
+    {
+        printf("Server closed the connection\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        perror("recv");
+    }
+}
+
+void menu(int sockfd, char *command)
+{
+    while (1)
+    {
+        printf("--------------------\nAvailable commands:\n");
+        printf("login <username> <password>\n");
+        printf("create_user <username> <age> <Gender (M/F)> <password>\n\n");
+        printf("commands for logged in users:\n");
+        printf("list_groups\n");
+        printf("join_group <group_name>\n--------------------\n");
+        printf("Enter command: ");
+        if (fgets(command, BUFFER_SIZE, stdin) == NULL)
+        {
+            break;
+        }
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strncmp(command, "exit", 5) == 0)
+        {
+            return;
+        }
+
+        if (strncmp(command, "login", 5) == 0)
+        {
+            handle_login_command(sockfd, command);
+        }
+        else if (strncmp(command, "join_group", 10) == 0)
+        {
+            handle_join_command(sockfd, command);
+        }
+        else if (strncmp(command, "list_groups", 11) == 0)
+        {
+            if (strlen(current_user) == 0)
+            {
+                printf("You must be logged in to list groups.\n\n");
+            }
+            else
+            {
+                send_command(sockfd, command);
+            }
+        }
+        else
+        {
+            send_command(sockfd, command);
+        }
     }
 }
 
@@ -107,47 +242,10 @@ int main(int argc, char *argv[])
     }
 
     char command[BUFFER_SIZE];
-    char current_user[50] = ""; // Store the current user's username
 
-    while (1)
-    {
-        printf("Available commands:\n");
-        printf("login <username> <password>\n");
-        printf("create_user <username> <age> <Gender (M/F)> <password>\n\n");
-        printf("commands for logged in users:\n");
-        printf("list_groups\n");
-        printf("join_group <group_name>\n\n");
-        printf("Enter command: ");
-        if (fgets(command, sizeof(command), stdin) == NULL)
-        {
-            break;
-        }
-        command[strcspn(command, "\n")] = '\0'; // Remove newline character
+    menu(sockfd, command);
 
-        // Check if the command is a login command
-        if (strncmp(command, "login", 5) == 0)
-        {
-            handle_login_command(sockfd, command, current_user);
-        }
-        else if (strncmp(command, "join_group", 10) == 0)
-        {
-            // Automatically append the current user to the join_group command
-            if (strlen(current_user) == 0)
-            {
-                printf("You must be logged in to join a group.\n");
-            }
-            else
-            {
-                char join_command[BUFFER_SIZE];
-                snprintf(join_command, sizeof(join_command), "join_group %s %s", current_user, command + 11);
-                send_command(sockfd, join_command);
-            }
-        }
-        else
-        {
-            send_command(sockfd, command);
-        }
-    }
+    printf("exiting application...\n\n");
 
     close(sockfd);
     return 0;
